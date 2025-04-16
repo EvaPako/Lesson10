c8d4b740d587f22ee95ccb5ec414a97f5ce48b29
EvaPako <lenapanenko@yandex.ru>
2025-04-16 23:11:56 +0300

    1

---
+++ src/main/java/mts/HomePage.java
@@ -0,0 +1,126 @@
+package mts;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+public class HomePage {
+    public final WebDriver driver;
+
+    public By onlinePayBlock = By.xpath(
+            "//*[@id='pay-section']//h2");
+
+    private final By buttonCookieAgree = By.id("cookie-agree");
+
+    public By visaLogo = By.cssSelector("img[alt='Visa']");
+    public By vByVisaLogo = By.cssSelector("img[alt='Verified By Visa']");
+    public By masterCardLogo = By.cssSelector("img[alt='MasterCard']");
+    public By MasterCardSCLogo = By.cssSelector("img[alt='MasterCard Secure Code']");
+    public By belcardLogo = By.cssSelector("img[alt='Белкарт']");
+
+
+    public final By [] paymentLogos = {
+            visaLogo,
+            vByVisaLogo,
+            masterCardLogo,
+            MasterCardSCLogo,
+            belcardLogo};
+
+    private final By ServiceInfo = By.linkText("Подробнее о сервисе");
+
+    private final By serviceDropdown = By.xpath("//*[@id='pay-section']//button/span[2]");
+    private final By phoneField = By.xpath("//*[@id='connection-phone']");
+    private final By sumField = By.xpath("//*[@id='connection-sum']");
+    private final By payButton = By.xpath("//*[@id='pay-connection']/button");
+
+
+    public final By uslugiSvyazi = By.xpath( "//*[@id='pay-section'']//ul/li[1]/p");
+    public final By homeInternet = By.xpath( "//*[@id='pay-section'']//ul/li[2]/p");
+    public final By rassrochka = By.xpath( "//*[@id='pay-section']//ul/li[3]/p");
+    public final By sadolzhnost = By.xpath("//*[@id='pay-section']//ul/li[4]/p");
+
+
+    public String [][] elementsPayForm = new String[][]{
+            {"connection-phone", "Номер телефона"},
+            {"connection-sum", "Сумма"},
+            {"connection-email", "E-mail для отправки чека"},
+            {"internet-phone", "Номер абонента"},
+            {"internet-sum", "Сумма"}
+    };
+
+    public By sumFrame = By.cssSelector(".pay-description__cost > span");
+    public By sumButtonFrame = By.cssSelector(".card-page__card > button");
+    public By phoneNumberFrame = By.cssSelector(".pay-description__text > span");
+    public By cardNumberFrame = By.xpath("//app-card-input/form/div[1]/div[1]/app-input/div/div/div[1]/label");
+    public By srokDeistvia = By.xpath("//app-card-input/form/div[1]/div[2]/div[1]/app-input/div/div/div[1]/label");
+
+    private final By payDescriptionCost = By.xpath("//*[@class='pay-connection']/button");
+
+    public HomePage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void acceptCookies(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement cook = driver.findElement(buttonCookieAgree);
+        if (cook.isDisplayed()){
+            cook.click();
+        }
+
+    }
+
+    public void clickAboutService(){
+        driver.findElement(ServiceInfo).click();
+
+    }
+
+    public WebElement getWebElement(By elementLocator){
+        return driver.findElement(elementLocator);
+    }
+
+    public void enterPhone(String phone) {
+        WebElement phoneElement = driver.findElement(phoneField);
+        phoneElement.clear();
+        phoneElement.sendKeys(phone);
+    }
+
+    public void enterSum(String sum) {
+        WebElement sumElement = driver.findElement(sumField);
+        sumElement.clear();
+        sumElement.sendKeys(sum);
+    }
+
+    public void clickPay() {
+        driver.findElement(payButton).click();
+    }
+
+    public void navigateBack(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().back();
+        //acceptCookies();
+
+    }
+
+
+    public void selectService(String service) {
+        driver.findElement(serviceDropdown).sendKeys(service); // Выбор услуги из выпадающего списка
+    }
+
+    public String getPhonePlaceholder() {
+        return driver.findElement(phoneField).getAttribute("placeholder");
+    }
+
+    public String getSumPlaceholder() {
+        return driver.findElement(sumField).getAttribute("placeholder");
+    }
+}
+
+
+
+
+
---
+++ src/test/java/HomeTest.java
@@ -0,0 +1,135 @@
+import mts.HomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class HomeTest {
+    public static WebDriver driver;
+    private static HomePage homePage;
+
+
+    @BeforeSuite
+    public static void setUp() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.mts.by/");
+        homePage = new HomePage(driver);
+        homePage.acceptCookies();
+
+    }
+
+    @AfterSuite
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+        if (homePage.driver != null){
+            homePage.driver.quit();
+        }
+    }
+
+    @Test(priority = 0)
+    public void verifyText() {
+
+        WebElement webElement = homePage.getWebElement(homePage.onlinePayBlock);
+        Assert.assertEquals(webElement.getText(), "Онлайн пополнение\nбез комиссии");
+
+    }
+
+    @Test(priority = 1)
+    public void verifyLogotype() {
+
+        try {
+            for (By logo : homePage.paymentLogos) {
+            WebElement logoElement = homePage.getWebElement(logo);
+            Assert.assertTrue(logoElement.isDisplayed(), "Логотип не найден: " + logo);
+        }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            driver.quit();
+        }
+    }
+
+  @Test(priority = 2)
+    public void checkServiceLink() {
+        homePage.clickAboutService();
+        String actualUrl = homePage.driver.getCurrentUrl();
+        String expectedUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
+        Assert.assertEquals(actualUrl, expectedUrl, "URL не соответствует ожидаемому");
+        homePage.navigateBack();
+    }
+
+    @Test(priority = 3)
+    public void Input () {
+        homePage.enterPhone("297777777");
+        homePage.enterSum("250");
+        homePage.clickPay();
+    }
+
+    @Test(priority = 4)
+    public void testPlaceHolders(){
+
+        try {
+            for (String[] element : homePage.elementsPayForm) {
+                WebElement inputField = homePage.getWebElement(By.id(element[0]));
+                String placeholderValue = inputField.getAttribute("placeholder");
+                Assert.assertEquals(placeholderValue, element[1], "У элемента " + element[0] + " надпись незаполненого поля не соответствует ожидаемому");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            driver.quit();
+        }
+    }
+
+    @Test(priority = 5)
+    public void checkFrame() {
+
+        String sum = "250.00";
+        String phoneNumber = "297777777";
+
+        //Выбираем Услуги связи
+        homePage.enterPhone(phoneNumber);
+        homePage.enterSum(sum);
+        homePage.clickPay();
+
+        //Ожидание открытия окна оплаты
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Переключаемся на фрейм
+        WebElement frame1 = homePage.getWebElement(By.className("bepaid-iframe"));
+        homePage.driver.switchTo().frame(frame1);
+
+        //Проверяем отображение суммы, номера телефона и т.д.
+        String actualSumFrame = homePage.getWebElement(homePage.sumFrame).getText();
+        Assert.assertEquals(actualSumFrame, sum + " BYN", "В окне оплаты сумма отображается неверно");
+
+        String actualButtonSumFrame = homePage.getWebElement(homePage.sumButtonFrame).getText();
+        Assert.assertEquals(actualButtonSumFrame, "Оплатить " + sum + " BYN", "В окне оплаты сумма на кнопке отображается неверно");
+
+        String actualPhoneNumberFrame = homePage.getWebElement(homePage.phoneNumberFrame).getText();
+        Assert.assertEquals(actualPhoneNumberFrame, "Оплата: Услуги связи Номер:375" + phoneNumber, "В окне оплаты сумма отображается неверно");
+
+        String actualСardNumberFrame = homePage.getWebElement(homePage.cardNumberFrame).getText();
+        Assert.assertEquals(actualСardNumberFrame, "Номер карты", "Отображается неверно");
+
+        String actualSrokDeistvia = homePage.getWebElement(homePage.srokDeistvia).getText();
+        Assert.assertEquals(actualSrokDeistvia, "Срок действия", "Отображается неверно");
+
+        // Вернуться к основному контенту страницы:
+        homePage.driver.switchTo().defaultContent();
+    }
+}
+
+
+
